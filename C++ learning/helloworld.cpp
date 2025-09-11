a2eac1dad8c1f53257b89d7444865eaf51c42aab
jvk2105 <vinhkhang210507@gmail.com>
2025-09-11 17:41:49 +0700

    Day 1.2

--- C++ learning/helloworld.cpp
+++ C++ learning/helloworld.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
-using namespace std
+/*sybau*/
+using namespace std;
 int main() {
-        cout << "Hello World!";
+        cout << "Hello World!" << endl;
+        cout << "Name: Nguyen Lam Vinh Khang" << endl << "Date of birth: 21/05/2007" << endl;
+        cout << "2025 - 2007 = " << 2025 - 2007;
         return 0;
 }
