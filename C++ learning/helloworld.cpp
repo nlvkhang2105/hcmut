b829c203bc0a392772c07b406e216a491ac10fa6
jvk2105 <vinhkhang210507@gmail.com>
2025-09-22 20:40:34 +0700

    Day 6.1

--- C++ learning/helloworld.cpp
+++ C++ learning/helloworld.cpp
@@ -8,15 +8,15 @@ int main() {
         short current_year;
         cout << "Please enter your name: ";
         getline(cin, name);
-        if (name == "Nguyễn Hiểu Lam" || name == "Nguyen Hieu Lam" || name == "hlam" || name == "nguyen hieu lam") {
+        cout << "Please enter your date of birth: ";
+        cin >> date_of_birth;
+        if (name == "Nguyễn Hiểu Lam" || name == "Nguyen Hieu Lam" || name == "hlam" || name == "nguyen hieu lam" || date_of_birth == "20/08/2008" || date_of_birth == "20-08-2008") {
                 cout << "Hi baby!" << endl;
                 cout << "I miss you so much!" << endl;
-                cout << "This is a program made by me" << endl;
+                cout << "This is a program made by NLVK" << endl;
                 system("pause");
                 return 0;
         }
-        cout << "Please enter your date of birth: ";
-        cin >> date_of_birth;
         cout << "Please enter current year: ";
         cin >> current_year;  
         cout << "Name: " << name << endl << "Date of birth: " << date_of_birth << endl;
