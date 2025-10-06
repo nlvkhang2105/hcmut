de85b6b3fb2b8b4c7ae208a39b6c6045389381ca
jvk2105 <vinhkhang210507@gmail.com>
2025-10-06 22:04:51 +0700

    Day 10.5

---
+++ C++ learning/chap2_ex3.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main() {
+    int a, b;
+    int sum;
+    a = 0;
+    b = 0;
+    sum = 0; 
+    cin >> a >> b;
+    sum = a + b;
+    cout << "a + b = " << sum << endl;
+    return 0;
+}
