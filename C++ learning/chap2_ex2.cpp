fc6d4c64912f8c664f0644d9a3db42f334afabb2
jvk2105 <vinhkhang210507@gmail.com>
2025-10-06 21:41:04 +0700

    Day 10.4

---
+++ C++ learning/chap2_ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream >
+using namespace std;
+
+int main () {
+cout << (2 + 4 * 3 - 5) << '\n';
+cout << (10 - 8 / 2 + 6 * 2 - 3) << '\n';
+cout << (0 / 5 * 3 + 4 * 2 - 12 / 3 * 2 + 7 - 6 / 2) << '\n';
+cout << (25 - 7 * 4 % 3 + 12 / 2) << '\n';
+cout << (9 * 4 % 7 * 6 / 3 + 8 % 5 * 2 - 11 / 2) << '\n';
+return 0;
+}
