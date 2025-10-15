76a7b976fedb1b62e71deac3e33940df2ab6bc55
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 18:57:43 +0700

    Day 13.5

---
+++ C++ learning/Chap 3/Exercise 2/ex2.2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    string name;
+    string display_name = "* Name: ";
+    string display_age = "* Age: ";
+    string age;
+    int width_name = 0;
+    int width_age = 0;
+    int starcount = 0;
+    cout << "Please enter your name: ";  
+    getline(cin,name);
+    cout << "Please enter your age: ";
+    cin >> age;
+    for(int i = 0; i <= name.length() + 20; i++ ){
+        cout << "*";
+        starcount ++;
+    }
+    cout << endl;
+    width_name = starcount - (display_name.length() + name.length());
+    width_age = starcount - (display_age.length() + age.length());
+    //cout << "*" << " " << "Name: " << name << setw(name.length() - ) << "*" << endl;
+    //cout << setw(0) << "*" << " " << "Age: " << age << setw(name.length()) << "*" << endl;
+    cout << display_name << name << setw(width_name) << "*" << endl;
+    cout << display_age << age << setw(width_age) << "*" << endl;
+    cout << setw(0);
+    for(int i = 0; i <= name.length() + 20; i++ ){
+        cout << "*";
+    }
+    cout << endl;
+    system("pause");
+}
