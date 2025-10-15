ce4e6c24f26d59c9669b85ac40a4929b1ed00b6a
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 23:45:45 +0700

    Day 13.22

--- C++ learning/Chap 3/Exercise 3/ex3.4.cpp
+++ C++ learning/Chap 3/Exercise 3/ex3.4.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 int main() {
     const double g = 9.81; 
-    double waterDepth;     
-    double angleInDegrees; 
+    double water_depth;     
+    double angle_degrees; 
     cout << "Enter the water depth (h) in meters: ";
-    cin >> waterDepth;
+    cin >> water_depth;
     cout << "Enter the seabed slope angle (alpha) in degrees: ";
-    cin >> angleInDegrees;
-    double angleInRadians = angleInDegrees * (M_PI / 180.0);
-    double waveSpeed = sqrt(g * waterDepth * tan(angleInRadians));
-    cout << "The approximate speed of the tsunami is: " << fixed << setprecision(2) << waveSpeed << " m/s.";
+    cin >> angle_degrees;
+    double angle_rad = angle_degrees * (M_PI / 180.0);
+    double wave_speed = sqrt(g * water_depth * tan(angle_rad));
+    cout << "The approximate speed of the tsunami is: " << fixed << setprecision(2) << wave_speed << " m/s.";
     return 0; 
 }
