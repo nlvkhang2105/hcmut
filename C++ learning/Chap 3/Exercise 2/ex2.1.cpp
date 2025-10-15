69458bfccd1e7141ad55e5ab7f3848b6c33fd6df
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 19:29:01 +0700

    Day 13.9

---
+++ C++ learning/Chap 3/Exercise 2/ex2.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    cout << 1 << endl;
+    cout << 2 << " " << 3 << endl;
+    cout << 4 << " " << 5 << " " << 6 << endl;
+    cout << 7 << " " << 8 << " " << 9 << " " << 10 << endl;
+    system("pause");
+}
