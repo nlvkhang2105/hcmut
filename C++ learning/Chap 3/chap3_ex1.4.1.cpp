8b6fd667db64202f193147885f7553acf57da9a7
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 15:55:57 +0700

    Day 13.2

---
+++ C++ learning/Chap 3/chap3_ex1.4.1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    int a = 2; 
+    int b = 0; 
+    int c = 2; 
+    int d = 5;
+    cout << ( a++) -- << endl; 
+    cout << ++( a++);
+}
---
+++ C++ learning/Chap 3/chap3_ex41.4.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    int a = 2; 
+    int b = 0; 
+    int c = 2; 
+    int d = ( a++) + ( a++); 
+    cout << " d: " << d;
+}
