ec073ca6208ffa8c981e76029eb793cdb25cea30
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 21:32:40 +0700

    Day 13.14

--- C++ learning/Chap 3/Exercise 2/ex2.3.cpp
+++ C++ learning/Chap 3/Exercise 2/ex2.3.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
-int main(){
-    float num;
-    cout << "Input your number:";
-    cin >> num;
-    for(int rows = 0; rows <= 4; rows++){
-        
+int main() {
+    double num;
+    std::cout << "Input your number: ";
+    std::cin >> num;
+    std::cout << std::fixed << std::setprecision(1);
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 0; j < 4 - i; ++j) {
+            std::cout << "   ";
+        }
+        for (int k = 0; k < i; ++k) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
     }
+    return 0;
 }
