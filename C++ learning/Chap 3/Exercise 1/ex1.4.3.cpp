8a0c2fa8c34153f94378ba0a68cac64d565ec5f5
jvk2105 <vinhkhang210507@gmail.com>
2025-10-15 16:51:59 +0700

    Day 13.4

---
+++ C++ learning/Chap 3/Exercise 1/ex1.4.3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    //int a = 2; 
+    int b = 0; 
+    int c = 2; 
+    int d = 5;
+    /*const*/ int a = 3; 
+    a++;
+    cout << "a: " << a;
+}
