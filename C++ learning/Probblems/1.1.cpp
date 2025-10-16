539b9cfa3bf161ed2748f4506c362d41961c8633
jvk2105 <vinhkhang210507@gmail.com>
2025-10-16 09:35:33 +0700

    Day 14

---
+++ C++ learning/Probblems/1.1.cpp
@@ -0,0 +1,14 @@
+#include<iomanip>
+#include<iostream>
+
+using namespace std;
+int main(){
+    char grade = 'B';
+    int p = 8;
+    int q = 15;
+    int r = 20;
+    int s = 25;
+    float t = 19.5;
+    cout << (p > q);
+    return 0;
+}
---
+++ C++ learning/Probblems/1.2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    int wind = 130;
+    int pressure = 9.85;
+    int rain = 220;
+    float river = 4.6;
+    bool tide = true;
+    bool strongstorm = (wind >= 120) && (pressure <= 980);
+    //cin >> rain >> river >> tide;
+    bool floodrisk = (rain > 200) || ((river > 4.5) && tide);
+    bool safe = (!strongstorm && !floodrisk);
+    cout << "Floodrisk: "  << floodrisk << endl;
+    cout << "Strongstorm: " << strongstorm << endl;
+    cout << "Safety: " << safe;
+    return 0;
+}
