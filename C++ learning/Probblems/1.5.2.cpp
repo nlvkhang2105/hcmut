d88c0006fa444b221ec7dd2639466571b10dc923
jvk2105 <vinhkhang210507@gmail.com>
2025-10-16 15:24:06 +0700

    Day 14.5

--- C++ learning/Probblems/1.5.2.cpp
+++ C++ learning/Probblems/1.5.2.cpp
@@ -3,14 +3,28 @@
 
 using namespace std;
 int main(){
+    const float POUNDS_TO_KILO = 0.45359237;
+    const float INCHES_TO_METERS = 0.0254;
     float weight_pounds;
     float height_inches;
-    cout << "Enter your height in inches: ";
-    cin >> height_inches;
-    cout << "Enter your weight in pounds: ";
-    cin >> weight_pounds;
-    const float weight_kilo = weight_pounds * 0.45359237;
-    const float height_meters = height_inches * 0.0254;
+    float weight_kilo;
+    float height_meters;
+    bool unit_chooser;
+    cout << "Choose your unit (0 for Metric, 1 for Imperial):";
+    cin >> unit_chooser;
+    if (unit_chooser == 1){
+        cout << "Enter your height in inches: ";
+        cin >> height_inches;
+        cout << "Enter your weight in pounds: ";
+        cin >> weight_pounds;
+        weight_kilo = weight_pounds * POUNDS_TO_KILO;
+        height_meters = height_inches * INCHES_TO_METERS;
+    } else {
+        cout << "Enter your height in meters: ";
+        cin >> height_meters;
+        cout << "Enter your weight in kilogram: ";
+        cin >> weight_kilo;
+    }
     float BMI = weight_kilo / (height_meters*height_meters);
     cout << "Your BMI is: " << BMI << endl;
     if (BMI < 18.5) {
