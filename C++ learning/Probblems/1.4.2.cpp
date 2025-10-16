03f4066eb09396e04b1d2642a5c86b8817de5136
jvk2105 <vinhkhang210507@gmail.com>
2025-10-16 15:08:03 +0700

    Day 14.4

--- C++ learning/Probblems/1.4.2.cpp
+++ C++ learning/Probblems/1.4.2.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main(){
     string name;
     string test;
-    char s[a];
+    string s[10000];
     getline(cin, name);
     for(int i = 0; i <= name.length(); i++){
-        test = string(name, i);
-        s[i] = test
+        test = string(name,i,1);
+        s[i] = test;
+    }
+    for(int i = name.length(); i >= 0; i--){
+        cout << s[i];
     }
     return 0;
 }
