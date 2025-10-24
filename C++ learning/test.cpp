f163d18913a8591df05f3d5c252423283bd3973f
jvk2105 <vinhkhang210507@gmail.com>
2025-10-24 09:55:16 +0700

    Day 16

---
+++ C++ learning/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        if (i == 2) {
+            continue;
+        }
+        std::cout << i << " ";
+    }
+    return 0;
+}
