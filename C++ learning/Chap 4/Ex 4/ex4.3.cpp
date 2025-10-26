d5bbca02f29189783160ae75ad318435c4e67ea4
jvk2105 <vinhkhang210507@gmail.com>
2025-10-26 10:15:29 +0700

    Day 17.1

--- C++ learning/Chap 4/Ex 4/ex4.3.cpp
+++ C++ learning/Chap 4/Ex 4/ex4.3.cpp
@@ -13,7 +13,7 @@ int main(){
     //cin >> d;
     if(a == 90 && b == 90 && c == 90){
         cout << "Your shape is a rectangle";
-    } else if( ((b == c) && (a == 180 - a)) || ((b == c) || (a == 180 - c))){
+    } else if(((b == c) && (a == 180 - c))){
         cout << "Your shape is an parralellogram";
     } else if(a == 90 && b == 90 && ((c != 180 - a) || (c != 180 - b))){
         cout << "Your shape is a right trapezoid";
