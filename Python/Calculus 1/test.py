ab1c858ffd64ad1d90c046da3b6ac38d8deaa89e
jvk2105 <vinhkhang210507@gmail.com>
2025-10-30 21:05:36 +0700

    Day 20.3

--- Python/Calculus 1/test.py
+++ Python/Calculus 1/test.py
@@ -1,2 +1,15 @@
 import math
-print("Hello World")
+import re
+def trig(expression):
+    trig = r"arcsin|arccos|arctan"
+    number = r'\d+'
+    trignum = re.findall(number, expression)
+    trigex = re.findall(trig , expression)
+    print(trigex)
+    print(trignum)
+expression = input("Input your expression: ")
+if 'arccos' in expression:
+    trig(expression)
+if 'arcsin' in expression:
+    trig(expression)
+#print(result)
