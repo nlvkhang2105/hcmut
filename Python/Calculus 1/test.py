05ce1d7fb4642838259eb071d5ce0d842d159101
jvk2105 <vinhkhang210507@gmail.com>
2025-11-01 09:57:25 +0700

    Day 21

--- Python/Calculus 1/test.py
+++ Python/Calculus 1/test.py
@@ -6,10 +6,23 @@ def trig(expression):
     trignum = re.findall(number, expression)
     trigex = re.findall(trig , expression)
     print(trigex)
+    for x in trigex:
+        if x == "arccos":
+            y = math.acos(trignum[i])
     print(trignum)
 expression = input("Input your expression: ")
-if 'arccos' in expression:
-    trig(expression)
-if 'arcsin' in expression:
-    trig(expression)
+expression_updated = expression
+match expression:
+    case _ if 'arcsin' in expression:
+        expression_updated = expression.replace("sin", "math.sin")
+print(expression_updated)
+print(eval(expression_updated))
+#if 'asin' in expression:
+#    raise TypeError("arcsin not asin")
+#if 'arccos' in expression:
+#    trig(expression)
+#if 'arcsin' in expression:
+#    trig(expression)
+#if 'arctan' in expression:
+#    trig(expression)
 #print(result)
---
+++ Python/Calculus 1/test2.py
@@ -0,0 +1,5 @@
+import math
+a = input()
+print(type(a))
+b = eval(a)
+print(b)
