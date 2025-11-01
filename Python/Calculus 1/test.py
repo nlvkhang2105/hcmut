97b6d11f37168837f16735a4218f2497f8786b4e
jvk2105 <vinhkhang210507@gmail.com>
2025-11-01 10:51:38 +0700

    Day 21.3

--- Python/Calculus 1/test.py
+++ Python/Calculus 1/test.py
@@ -1,22 +1,24 @@
 import math
 import re
 def prepare(expr):
-    expr = expr.replace('arcsin', 'math.asin', 1)
-    expr = expr.replace('arccos', 'math.acos', 1)
-    expr = expr.replace('arctan', 'math.atan', 1)
-    expr = expr.replace('sinh', 'math.sinh', 0)
-    expr = expr.replace('cosh', 'math.cosh', 0)
-    expr = expr.replace('tanh', 'math.tanh', 0)
-    expr = expr.replace('sin', 'math.sin', 0)
-    expr = expr.replace('cos', 'math.cos', 0)
-    expr = expr.replace('tan', 'math.tan', 0)
-    expr = expr.replace('ln', 'math.log')
-    expr = expr.replace('exp', 'math.exp')
-    expr = expr.replace('sqrt', 'math.sqrt')
-    expr = expr.replace('pi', 'math.pi')
-    expr = expr.replace('e', 'math.e')
+    expr = re.sub(r'\bsinh', 'math.sinh', expr) #r'' means a raw string — it tells Python not to treat \ as an escape character
+    expr = re.sub(r'\bcosh', 'math.cosh', expr) # => r'\b' is literally the characters \ and b
+    expr = re.sub(r'\btanh', 'math.tanh', expr) # => \b is a word boundary in regex.
+    expr = re.sub(r'\basinh', 'math.asinh', expr)
+    expr = re.sub(r'\bacosh', 'math.acosh', expr)
+    expr = re.sub(r'\batanh', 'math.atanh', expr)
+    expr = re.sub(r'\barcsin', 'math.asin', expr)
+    expr = re.sub(r'\barccos', 'math.acos', expr)
+    expr = re.sub(r'\barctan', 'math.atan', expr)
+    expr = re.sub(r'\bsin', 'math.sin', expr)
+    expr = re.sub(r'\bcos', 'math.cos', expr)
+    expr = re.sub(r'\btan', 'math.tan', expr)
+    expr = re.sub(r'\bexp', 'math.exp', expr)
+    expr = re.sub(r'\bln', 'math.log', expr)
+    expr = re.sub(r'\bsqrt', 'math.sqrt', expr)
+    expr = re.sub(r'\bpi', 'math.pi', expr)
+    expr = re.sub(r'\be', 'math.e', expr)
     return expr
-
 expression = input("Input your expression: ")
 
 try:
