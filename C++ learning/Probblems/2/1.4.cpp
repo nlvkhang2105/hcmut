8aaac2878ff9f67ecd1586af133ef4edcd5565ab
jvk2105 <vinhkhang210507@gmail.com>
2025-11-06 10:44:28 +0700

    Day 22.4

---
+++ C++ learning/Probblems/2/1.4.cpp
@@ -0,0 +1,22 @@
+#include<bits\stdc++.h>
+
+using namespace std;
+int main(){
+    int pass = 6967;
+    int attempt = 1;
+    int input;
+    while (attempt < 3){
+        cout << "Input:";
+        cin >> input;
+        if(input != pass){
+            cout << "NUH UH";
+            attempt++;
+        }
+        if(input == pass){
+            cout << "Good";
+            return 0;
+        }
+        cout << "Fuck off nigga";
+    }
+    return 0;
+}
