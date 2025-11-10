bd6190f76f62d72bcc6c8a9da397ffa305cdc9dd
jvk2105 <vinhkhang210507@gmail.com>
2025-11-10 19:45:23 +0700

    Day 23

---
+++ C++ learning/intro_to_computing_assignment.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    int slayerLevel;
+    int hp;
+    double breathingMastery;
+    int hasTalisman;
+    char timeOfDay;
+    int demonPresence;
+    int demonRank;
+    double swordSharpness;
+    int allyCount;
+    int bossHP;
+    int totalDamage;
+    int specialMoveReady;
+    cin >> slayerLevel >> hp >> breathingMastery >> hasTalisman >> timeOfDay >> demonPresence >> demonRank >> swordSharpness 
+        >> allyCount >> bossHP >> totalDamage >> specialMoveReady;
+    double power = (slayerLevel * 10) + (hp / 10) + (breathingMastery * 50);
+    string demonslayerPower; //scene 1 prerequistes
+    string gate_open;
+    if (power >= 120){ 
+        demonslayerPower = "Hashira";
+    } else if(80 <= power < 120 ){
+        demonslayerPower = "Elite";
+    } else if(power < 80){
+        demonslayerPower = "Novice";
+    }
+    cout << "[Scene 1] Rank: " << demonslayerPower << " (power = " << fixed << setprecision(1) << power << ")" << endl;  
+    if(hasTalisman == 0){ //scene 2 prerequisites
+        gate_open = "Denied: No talisman.";
+    } else if((timeOfDay != 'D') && (timeOfDay != 'N')){
+        gate_open = "Warning: invalid timeOfDay.";
+    } else if((timeOfDay == 'N') && (demonPresence == 1)){
+        gate_open = "Open silently.";
+    } else gate_open = "Open cautiously.";
+    cout << "[Scene 2] " << gate_open << endl;
+    string strat; //scene 3 prerequisites
+    double adv = (101- demonRank * 15) + swordSharpness * 0.4 + allyCount * 5; 
+    if(adv >= 100){
+        strat = "Engage head-on";
+    } else if(60 <= adv < 100){
+        strat = "Harass and probe";
+    } else if(adv < 60) {
+        strat = "Retreat and regroup";
+    } else strat = "Retreat and regroup";
+    cout << "[Scene 3] " << strat << " (adv = " << fixed << setprecision(1) << adv << ")" << endl;
+    double finalHP = bossHP - totalDamage; //scene 4 prerequisites
+    string final;
+    if(finalHP <= 0){
+        final = "Boss defeated!";
+    } else if((0 < finalHP <= 50) && (specialMoveReady == 1)){
+        final = "Use special move to finish!";
+    } else final = "Withdraw to heal.";
+    cout << "[Scene 4] " << final << " (finalHP = " << (int)finalHP << ")" << endl;
+    return 0;
+}
--- Python/Calculus 1/test2.py
+++ Python/Calculus 1/test2.py
@@ -1,69 +1,30 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import re
 import math
-expression=input("Enter the expression: ")
+import re
 def prepare(expr):
-    expr = re.sub(r'\bsinh', 'sinh', expr)
-    expr = re.sub(r'\bcosh', 'cosh', expr)
-    expr = re.sub(r'\btanh', 'tanh', expr)
-    expr = re.sub(r'\basinh', 'asinh', expr)
-    expr = re.sub(r'\bacosh', 'acosh', expr)
-    expr = re.sub(r'\batanh', 'atanh', expr)
-    expr = re.sub(r'\barcsin', 'arcsin', expr)
-    expr = re.sub(r'\barccos', 'arccos', expr)
-    expr = re.sub(r'\barctan', 'arctan', expr)
-    expr = re.sub(r'\bsin', 'sin', expr)
-    expr = re.sub(r'\bcos', 'cos', expr)
-    expr = re.sub(r'\btan', 'tan', expr)
-    expr = re.sub(r'\bexp', 'exp', expr)
-    expr = re.sub(r'\bln', 'log', expr)
-    expr = re.sub(r'\bsqrt', 'sqrt', expr)
-    expr = re.sub(r'\bpi', 'pi', expr)
-    expr = re.sub(r'\be', 'e', expr)
+    expr = re.sub(r'\bsinh', 'math.sinh', expr)
+    expr = re.sub(r'\bcosh', 'math.cosh', expr)
+    expr = re.sub(r'\btanh', 'math.tanh', expr)
+    expr = re.sub(r'\basinh', 'math.asinh', expr)
+    expr = re.sub(r'\bacosh', 'math.acosh', expr)
+    expr = re.sub(r'\batanh', 'math.atanh', expr)
+    expr = re.sub(r'\barcsin', 'math.asin', expr)
+    expr = re.sub(r'\barccos', 'math.acos', expr)
+    expr = re.sub(r'\barctan', 'math.atan', expr)
+    expr = re.sub(r'\bsin(?!h())', 'math.sin', expr)
+    expr = re.sub(r'\bcos(?!h())', 'math.cos', expr)
+    expr = re.sub(r'\btan(?!h())', 'math.tan', expr)
+    expr = re.sub(r'\bexp', 'math.exp', expr)
+    expr = re.sub(r'\bln', 'math.log', expr)
+    expr = re.sub(r'\bsqrt', 'math.sqrt', expr)
+    expr = re.sub(r'\bpi', 'math.pi', expr)
+    expr = re.sub(r'\be(?![a-zA-Z])', 'math.e', expr)
     expr = re.sub(r'\,', '.', expr)
     return expr
-
-evalcontext={
-    "pi": np.pi, 
-    "e": np.e,
-    "sin": np.sin,
-    "cos": np.cos,
-    "tan": np.tan,
-    "sinh": np.sinh, 
-    "cosh": np.cosh, 
-    "tanh": np.tanh,
-    "arcsin": np.arcsin, 
-    "arccos": np.arccos, 
-    "arctan": np.arctan,
-    "asinh": np.asinh, 
-    "acosh": np.acosh, 
-    "atanh": np.atanh,
-    "exp": np.exp,
-    "log": np.log,
-    "sqrt": np.sqrt
-}
+expression = input("Input your expression: ")
 
 try:
-    a = float(input("Enter a: "))
-    b = float(input("Enter b: "))
-    x = np.linspace(a,b,500)
     expression = prepare(expression)
-    evalcontext["x"] = x 
-    y = eval(expression,evalcontext)
-    y[y>100] = np.nan
-    y[y<-100] = np.nan
-    y[np.isinf(y)] = np.nan
-    plt.figure()
-    plt.plot(x,y,c = "r",linewidth = 1.5,label = "f(x)")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.title("The graph of f(x)")
-    plt.legend()
-    plt.xlim(a,b)
-    plt.grid()
-    plt.axhline(0,color = 'blue',linewidth = 1.5)
-    plt.axvline(0,color = 'blue',linewidth = 1.5)
-    plt.show()
+    result = eval(expression)
+    print("OUTPUT: ", result)
 except Exception as e:
     print("Error in expression: ", e)
