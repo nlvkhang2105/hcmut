30e18f4165920c4ae17428d1591b2ecc0a352a65
jvk2105 <vinhkhang210507@gmail.com>
2025-11-13 10:31:19 +0700

    Day 26.2

--- C++ learning/Intro to computing/Blended learning/ex3.cpp
+++ C++ learning/Intro to computing/Blended learning/ex3.cpp
@@ -6,26 +6,21 @@ using namespace std;
 void dectobin(int decimal){
     int bin[10000];
     int len = 0;
+    string out;
     while(decimal > 0){
         bin[len] = decimal % 2;
         decimal /= 2;
         len++;
     }
+    for(int i = len - 1; i >= 0; i--){
+        out += to_string(bin[i]);
+        cout << "The binary form is: " << out << '\n';
+    }
 }
 int main(){
     int decimal;
-    int binary;
-    string store;
-    string out;
     cout << "Enter a decimal: ";
     cin >> decimal;
-    int count = decimal;
-    for(int i = 0; i < count / 2; i++){
-        binary = decimal % 2;
-        decimal = decimal / 2;
-        store += to_string(binary);
-    } 
-    cout << store << '\n';
-    reverse(store.begin(),store.end());
+    dectobin(decimal);
     return 0;
 }
