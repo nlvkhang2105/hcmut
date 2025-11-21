92d8e39deabe130c5be7c8802b853db849394cb2
jvk2105 <vinhkhang210507@gmail.com>
2025-11-21 21:30:43 +0700

    Day 28.3

--- C++ learning/Intro to computing/Exercises/Chap 5/ex3.cpp
+++ C++ learning/Intro to computing/Exercises/Chap 5/ex3.cpp
@@ -8,20 +8,32 @@ double calculateAverage(double total, int count){
 }
 int main(){
     int n;
+    cout << "Enter the number of runners: ";
     cin >> n;
     if(n <= 0){
         cout << "Invalid number ";
+        return 0;
     }
-    double raceTimes[n-1];
-    double fastTime = 0;
+    double raceTimes[n];
     double totalTime = 0;
+    cout << "Enter the runner's times: ";
     for(int i = 0; i < n; i++){
         cin >> raceTimes[i];
-        if(raceTimes[i] > fastTime){
+    }
+    double fastTime = raceTimes[0];
+    double slowTime = raceTimes[0];
+    for(int i = 0; i < n; i++){
+        if(raceTimes[i] < fastTime){
             fastTime = raceTimes[i];
+        } 
+        if(raceTimes[i] > slowTime){
+            slowTime = raceTimes[i];
         }
         totalTime += raceTimes[i];
     }
     double avg = calculateAverage(totalTime,n);
+    cout << fixed << setprecision(2) << "Fastest time: " << fastTime << '\n';
+    cout << fixed << setprecision(2) << "Slowest time: " << slowTime << '\n';
+    cout << fixed << setprecision(2) << "Average time: " << avg << '\n';
     return 0;
 }
