c3a68eb27c9112202546aa48e862368e7e467568
jvk2105 <vinhkhang210507@gmail.com>
2025-11-24 21:54:47 +0700

    Day 30.1

--- C++ learning/Intro to computing/Assignment/Assignment 2/code.cpp
+++ C++ learning/Intro to computing/Assignment/Assignment 2/code.cpp
@@ -7,11 +7,11 @@ int main(){
     string pronounce;
     string alphabet[26] = {"a", "e", "i", "o", "u", "p", "k", "h", "l", "m", "n", "w", "A","E","I","O","U","P","K","H","L","M","N","W"," ","\'"};
     cout << "Enter your Hawaiian word: ";
-    cin >> pronounce;
-    for(int i = 0; i < word_in.length(); i++){
+    getline(cin, word_in);
+    for(int i = 0; i < word_in.length(); i++){ //check validity
         string test = word_in.substr(i,1);
         for(int j = 0; j < 26; j++){
-            if(test == alphabet[i]){
+            if(test == alphabet[j]){
                 break;
             }
             if(j == 25){
@@ -19,7 +19,45 @@ int main(){
                 return 0;
             }
         }
-        cout << "Valid";
+    }
+    cout << "Valid" << '\n';
+    for(int i = 0; i < word_in.length(); i++){
+        char test = word_in[i];
+        switch(test){ //check consonants
+            case 'p':
+            pronounce += 'p';
+            break;
+            case 'k':
+            pronounce += 'k';
+            break;
+            case 'h':
+            pronounce += 'h';
+            break;
+            case 'l':
+            pronounce += 'l';
+            break;
+            case 'm':
+            pronounce += 'm';
+            break;
+            case 'n':
+            pronounce += 'n';
+            break;
+            case 'w':
+            if(i == 0){
+                pronounce += 'w';
+            } else{
+                switch(word_in[i-1]){
+                case 'a': case 'u': case 'o':
+                pronounce += 'w';
+                break;
+                case 'i': case 'e':
+                pronounce += 'v';
+                break;
+            }
+            }
+            break;
+        }
+        
     }
     return 0;
 }
