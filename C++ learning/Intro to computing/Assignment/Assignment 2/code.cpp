29e84ef2ecdca21f567a217fa40e45966c586763
jvk2105 <vinhkhang210507@gmail.com>
2025-11-25 10:58:32 +0700

    Day 31

--- C++ learning/Intro to computing/Assignment/Assignment 2/code.cpp
+++ C++ learning/Intro to computing/Assignment/Assignment 2/code.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 #include<string>
+#include <algorithm>
+#include <cctype>
 using namespace std;
 
 int main(){
     string word_in;
     string pronounce;
-    string alphabet[26] = {"a", "e", "i", "o", "u", "p", "k", "h", "l", "m", "n", "w", "A","E","I","O","U","P","K","H","L","M","N","W"," ","\'"};
+    string alphabet[14] = {"a", "e", "i", "o", "u", "p", "k", "h", "l", "m", "n", "w", " ", "\'"};
     cout << "Enter your Hawaiian word: ";
     getline(cin, word_in);
+    transform(word_in.begin(), word_in.end(), word_in.begin(), [](unsigned char c){ return std::tolower(c); });
     for(int i = 0; i < word_in.length(); i++){ //check validity
         string test = word_in.substr(i,1);
         for(int j = 0; j < 26; j++){
@@ -25,39 +28,63 @@ int main(){
         char test = word_in[i];
         switch(test){ //check consonants
             case 'p':
-            pronounce += 'p';
-            break;
+                pronounce += 'p';
+                break;
             case 'k':
-            pronounce += 'k';
-            break;
+                pronounce += 'k';
+                break;
             case 'h':
-            pronounce += 'h';
-            break;
+                pronounce += 'h';
+                break;
             case 'l':
-            pronounce += 'l';
-            break;
+                pronounce += 'l';
+                break;
             case 'm':
-            pronounce += 'm';
-            break;
+                pronounce += 'm';
+                break;
             case 'n':
-            pronounce += 'n';
-            break;
+                pronounce += 'n';
+                break;
             case 'w':
-            if(i == 0){
-                pronounce += 'w';
-            } else{
-                switch(word_in[i-1]){
-                case 'a': case 'u': case 'o':
-                pronounce += 'w';
+                if(i == 0){
+                    pronounce += 'w';
+                } else{
+                    switch(word_in[i-1]){
+                    case 'a': case 'u': case 'o':
+                    pronounce += 'w';
+                    break;
+                    case 'i': case 'e':
+                    pronounce += 'v';
+                    break;
+                }
+                }
                 break;
-                case 'i': case 'e':
-                pronounce += 'v';
+        }
+        //bool vowelB4;
+        //bool vowelAft;
+        /*switch(word_in[i-1]){ //check vowel b4
+            case 'p': case 'k': case 'h': case 'l': case 'm': case 'n': case 'w':
+                vowelB4 = false;
                 break;
-            }
-            }
-            break;
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+                vowelB4 = true;
+                break;
+        }*/
+        bool group = false;
+        if(i + 1 < word_in.length()){
+            switch(word_in[i+1]){ //check vowel group
+            case 'p': case 'k': case 'h': case 'l': case 'm': case 'n': case 'w': 
+                group = false;
+                break;
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+                if(word_in[i] != word_in[i+1]){
+                    group = true;
+                } else group = false;
+                break;
+        }
         }
         
     }
+    cout << word_in;
     return 0;
 }
