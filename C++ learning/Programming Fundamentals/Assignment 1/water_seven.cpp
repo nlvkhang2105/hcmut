32276d2c45f5bf0ab74ed65d6a8cf99ea67d6b79
jvk2105 <vinhkhang210507@gmail.com>
2026-02-03 21:03:15 +0700

    Day 41

---
+++ C++ learning/Programming Fundamentals/Assignment 1/water_seven.cpp
@@ -0,0 +1,69 @@
+#include "water_seven.h"
+
+////////////////////////////////////////////////////////////////////////
+/// STUDENT'S ANSWER BEGINS HERE
+/// Complete the following functions
+/// DO NOT modify any parameters in the functions.
+////////////////////////////////////////////////////////////////////////
+//Task 0
+bool readInput(
+    const string &filename,
+    char character[FIXED_CHARACTER][MAX_NAME], int hp[FIXED_CHARACTER], 
+    int skill[FIXED_CHARACTER], int &shipHP, int &repairCost){
+        ifstream inFile("./opw_tc_01_input");
+        if(!inFile){
+            return false;
+        }
+        string line;
+        int i = 0;
+        while(getline(inFile,line)){
+            
+            i++;
+        }
+
+        inFile.close();
+        return false;
+}
+
+
+
+// Task 1
+int damageEvaluation(int shipHP, int repairCost){
+    return 0;
+}
+
+// Task 2
+int conflictSimulation(
+    char character[FIXED_CHARACTER][MAX_NAME], int hp[FIXED_CHARACTER], int skill[FIXED_CHARACTER],
+    int shipHP, int repairCost){
+        return 0;
+    }
+
+// Task 3
+void resolveDuel(
+    char character[FIXED_CHARACTER][MAX_NAME], int hp[FIXED_CHARACTER], int skill[FIXED_CHARACTER],
+    int conflictIndex, int repairCost, char duel[FIXED_CHARACTER][MAX_NAME]){
+        //TODO: Output assign to duel parameter
+    }
+
+// Task 4
+void decodeCP9Message(char character[FIXED_CHARACTER][MAX_NAME], 
+    int hp[FIXED_CHARACTER], int skill[FIXED_CHARACTER], int conflictIndex, 
+    int repairCost, char cipherText[], char resultText[]){
+        //TODO: Output assign to resultText parameter
+    }
+
+// Task 5
+int analyzeDangerLimit(int grid[MAX_GRID][MAX_GRID], int rows, int cols){
+    return 0;
+}
+
+
+bool evaluateRoute(int grid[MAX_GRID][MAX_GRID], int rows, int cols, int dangerLimit){
+    return false;
+}
+
+
+////////////////////////////////////////////////
+/// END OF STUDENT'S ANSWER
+////////////////////////////////////////////////
