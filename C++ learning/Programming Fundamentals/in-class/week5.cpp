a99532aec6f1d4d106ee89627ea8f4d4d62587ab
jvk2105 <vinhkhang210507@gmail.com>
2026-03-02 13:25:25 +0700

    Day 49

---
+++ C++ learning/Programming Fundamentals/in-class/week5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int prime[n];
+    int index = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j < i; j++){
+            bool primeNum = true;
+            int temp = i % j;
+            if(i % j == 0){
+                primeNum = false;
+            }
+            if(primeNum){
+                cout << i << '\n';
+            }
+        }
+    }
+    return 0;
+}
